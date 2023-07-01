140a7af45dd03c715ef33b2683ed6f118a5cbc67
CaireneA <>
2023-07-01 14:55:58 +0000

    Add add_expense function to take user input

--- run.py
+++ run.py
@@ -13,6 +13,26 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('expenses-tracker')
 
 expenses = SHEET.worksheet('expenses')
-data = expenses.get_all_values()
 
-print(data)
+def add_expense():
+    """    
+    This function asks the user to input the amount, category, and date of an expense.
+    
+    Returns:
+        amount (str): The amount of the expense entered by the user.
+        category (str): The category of the expense entered by the user.
+        date (str): The date of the expense entered by the user in YYYY-MM-DD format.
+    """
+    
+    print('Welcome to your personal Expenses Tracker')
+    
+    # expense details
+    amount = input("Enter the amount: $")
+    category = input("Enter the category: ").strip().capitalize() # clear white space
+    # category = cat.capitalize()
+    date = input("Enter the date (YYYY-MM-DD): ")
+    
+    return [amount, category, date]
+
+result = add_expense()
+print(result)
